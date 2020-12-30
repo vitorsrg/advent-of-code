3f58dd988e7555dc166a47936dc36118cb25744c
Vitor SRG <vitorssrg@gmail.com>
2020-12-30 03:08:36 -0300

    Add 2020d03 solution

---
+++ 2020/d03/main.clj
@@ -0,0 +1,48 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; domain:    Advent of Code 2020
+;; challenge: Day 3: Toboggan Trajectory
+;; url:       https://adventofcode.com/2020/day/3
+;; author:    Vitor SRG (vitorssrg@gmail.com)
+;; date:      2020-12-30
+;; execution: $ lein run -m main
+;; example:
+;;            $ lein run -m main
+;;            part 1 276
+;;            part 2 7812180000
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(ns main
+  ; (:require [clojure.core.matrix])
+  )
+
+(defn parse-map-line [line]
+  (nth (re-find #"(\S+)" line) 1))
+
+(defn tree-map-path-tree-count [tree-map [right-step down-step]]
+  (let [width (count (get tree-map 0))
+        height (count tree-map)
+        path (for [i (range 0 height down-step)
+                   :let [j (mod (* (/ i down-step) right-step) width)]]
+               [i j])
+        path-content (map #(get-in tree-map %) path)
+        path-tree-count (count (filter #(= \# %) path-content))]
+    path-tree-count))
+
+(defn -main [& args]
+
+  (let [input-file-name "./input.txt"
+        input-lines (clojure.string/split-lines (slurp input-file-name))
+        tree-map (vec (map parse-map-line input-lines))]
+
+    ;; part 1
+    (let [path-tree-count (tree-map-path-tree-count tree-map [3 1])]
+      (println "part 1" path-tree-count))
+
+    ;; part 2
+    (let [path-slopes [[1 1] [3 1] [5 1] [7 1] [1 2]]
+          path-slopes-tree-counts (zipmap path-slopes
+                                          (map #(tree-map-path-tree-count tree-map %)
+                                               path-slopes))
+          argmin-path-slope-tree-count (apply min-key #(get path-slopes-tree-counts %) path-slopes)
+          path-slopes-tree-count-product (apply * (vals path-slopes-tree-counts))]
+      (println "part 2" path-slopes-tree-count-product))))
---
+++ 2020/d03/project.clj
@@ -0,0 +1,12 @@
+(defproject aoc2020d01 "0.0.0"
+  ;; :description "FIXME: write description"
+  ;; :url "http://example.com/FIXME"
+  ;; :license {:name "Eclipse Public License - v 1.0"
+  ;;           :url "http://www.eclipse.org/legal/epl-v10.html"
+  ;;           :distribution :repo
+  ;;           :comments "same as Clojure"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 ;; [net.mikera/core.matrix "0.50.0"]
+                 ]
+  :source-paths ["."]
+  :main main)
